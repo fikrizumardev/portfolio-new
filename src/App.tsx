899881b3415cca04058b9475aad780733ef1b008
Fikri Zumar <muhammad.fikri.azzumardi@gmail.com>
2026-04-07 21:29:32 +0700

    Tambahan contact wa dengan icon mengambang

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Portfolio from './components/Portfolio';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import WhatsAppButton from './components/WhatsAppButton';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Skills />
       <Contact />
       <Footer />
+      <WhatsAppButton />
     </div>
   );
 }
